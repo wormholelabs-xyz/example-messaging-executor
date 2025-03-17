03b5c83a20060bd0d339e3dfdc5efa60d44423f3
Bruce Riley <bruce@wormholelabs.xyz>
2025-03-17 14:29:36 -0500

    evm: Add IVaaV1Receiver

---
+++ evm/src/interfaces/IVaaV1Receiver.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.19;
+
+/// @notice Any contract that wishes to receive V1 VAAs from the executor needs to implement `IVaaV1Receiver`.
+interface IVaaV1Receiver {
+    /// @notice Receive an attested message from the executor relayer.
+    /// @param msg The attested message payload.
+    function receiveMessage(bytes memory msg) external payable;
+}
