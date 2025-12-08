7a5e8c81bf5c531b35ca596bac50f386a853641c
Bengt Lofgren <bengtlofgren8@gmail.com>
2025-12-08 15:14:17 +0000

    delete comparison programs

--- svm/programs/executor-quoter-anchor/src/instructions/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod get_quote;
-pub mod initialize;
-pub mod update_chain_info;
-pub mod update_quote;
-
-pub use get_quote::*;
-pub use initialize::*;
-pub use update_chain_info::*;
-pub use update_quote::*;
--- svm/programs/executor-quoter-native/src/instructions/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Instruction handlers for the ExecutorQuoter program (native solana_program version).
-
-pub mod get_quote;
-pub mod initialize;
-pub mod update_chain_info;
-pub mod update_quote;
